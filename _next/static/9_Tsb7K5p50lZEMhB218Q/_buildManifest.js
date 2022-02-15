c39f6d2354d807d0aeab6e085f8ad288b8fbb943
변대웅 <dwbyun@studiomate.kr>
2022-02-15 11:04:58 +0900

    Deploy Next.js to gh-pages

---
+++ _next/static/9_Tsb7K5p50lZEMhB218Q/_buildManifest.js
@@ -0,0 +1,29 @@
+;(self.__BUILD_MANIFEST = (function (s, a) {
+  return {
+    __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+    '/': ['static/chunks/pages/index-f48495453757eecd.js'],
+    '/404': ['static/chunks/pages/404-2c141aebd0a30053.js'],
+    '/_error': ['static/chunks/pages/_error-34b1906f689dcf74.js'],
+    '/about': [s, a, 'static/chunks/pages/about-77853515a8f3efd8.js'],
+    '/blog': ['static/chunks/pages/blog-c1e1094293bde291.js'],
+    '/blog/page/[page]': ['static/chunks/pages/blog/page/[page]-e4f7cf84ef16d362.js'],
+    '/blog/[...slug]': [s, a, 'static/chunks/pages/blog/[...slug]-4c61555a4fc2f35d.js'],
+    '/projects': ['static/chunks/pages/projects-5e0fe7164b20c07b.js'],
+    '/tags': ['static/chunks/pages/tags-98a3831e97c01cc9.js'],
+    '/tags/[tag]': ['static/chunks/pages/tags/[tag]-5a36e02035953532.js'],
+    sortedPages: [
+      '/',
+      '/404',
+      '/_app',
+      '/_error',
+      '/about',
+      '/blog',
+      '/blog/page/[page]',
+      '/blog/[...slug]',
+      '/projects',
+      '/tags',
+      '/tags/[tag]',
+    ],
+  }
+})('static/chunks/390-b96e5ad1af755d6b.js', 'static/chunks/620-45d8b03e435051c9.js')),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
