09c5d05b8f1637429cf966e22f1f6175eeaf6844
daewoong-byun <dwbyun@studiomate.kr>
2022-02-16 14:59:28 +0900

    test

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -9,8 +9,6 @@ import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import { useTheme } from 'next-themes'
 
-const prefix = process.env.NODE_ENV === 'production' ? 'https://studiomate.github.io/tech-blog' : ''
-
 const LayoutWrapper = ({ children }) => {
   const { theme, setTheme, resolvedTheme } = useTheme()
 
@@ -19,7 +17,7 @@ const LayoutWrapper = ({ children }) => {
       <div className="flex h-screen flex-col justify-between">
         <header className="flex items-center justify-between py-10">
           <div>
-            <Link href={prefix + '/'} aria-label={siteMetadata.headerTitle}>
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
               <div className="flex items-center justify-between">
                 <div className="mr-3">{theme === 'light' ? <Logo /> : <WhiteLogo />}</div>
               </div>
@@ -30,7 +28,7 @@ const LayoutWrapper = ({ children }) => {
               {headerNavLinks.map((link) => (
                 <Link
                   key={link.title}
-                  href={prefix + link.href}
+                  href={link.href}
                   className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
                 >
                   {link.title}
